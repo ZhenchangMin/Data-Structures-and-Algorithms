f8e6409927d7012df2a91bab7ac108cbd09a4dd5
241880334 <241880334@smail.nju.edu.cn>
2025-09-16 12:44:34 +0800

    Update assignments.

--- In-school lessons/Assignments/hw01/t3.cpp
+++ In-school lessons/Assignments/hw01/t3.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main(){
-    int n, k;
-    cin >> n >> k;
+typedef struct node{
+    int value;
+    int index;
+} Node;
+
+void execute(vector<Node>& nodes, int quiry){
     
+}
+void print(vector<Node>& nodes){
+    for(int i = 1; nodes[i] ; i++){
+        cout << nodes[i].value << " ";
+    }
+    cout << endl;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--){
+        int n, q;
+        cin >> n >> q;
+        vector<Node> nodes(n+1);
+        for(int i = 1; i <= n; i++){
+            cin >> nodes[i].value;
+            nodes[i].index = i;
+        }
+        while(q--){
+            int quiry;
+            cin >> quiry;
+            execute(nodes, quiry);
+        }
+        print(nodes);
+    }
 }
