7d0835b44c6d5e8df57f9f9fbf900f0275165c9f
241880334 <241880334@smail.nju.edu.cn>
2025-09-18 10:13:48 +0800

    Finish hw01.

--- In-school lessons/Assignments/hw01/t4_final.cpp
+++ In-school lessons/Assignments/hw01/t4_final.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -19,34 +20,31 @@ int main()
         }
         int count = 0;
         int minHeight = a[0];
-        int maxIntermidiate = 0;
+        int maxMid = 0;
         for (int i = 0; i < n; i++)
         {
-            maxIntermidiate = 0;
-            minHeight = a[i];
+            const int heightI = a[i];
+            maxMid = 0;
             for (int j = i + 1; j < n; j++)
             {
-                minHeight = a[i]; // 两边更小的那一个楼房高度设为minHeight
-                if (a[j] <= minHeight)
-                {
-                    minHeight = a[j];
-                }
-                if (a[j - 1] > maxIntermidiate && j != i + 1)
-                {
-                    maxIntermidiate = a[j - 1];
-                }
+                minHeight = min(heightI, a[j]); // 两边更小的那一个楼房高度设为minHeight
                 if (j == i + 1)
                 {
                     count++;
+                    maxMid = 0;
                     continue;
                 } // 如果两楼相邻，直接加加
-                else if (maxIntermidiate <= minHeight)
-                {
-                    count++;
-                } // 如果两边之间的最大值小于minHeight，加加
                 else
                 {
-                    continue;
+                    maxMid = max(maxMid, a[j - 1]);
+                    if (maxMid <= minHeight)
+                    {
+                        count++;
+                    } // 如果两边之间的最大值<=minHeight，加加
+                    else if (maxMid > minHeight && minHeight == heightI)//如果已经比现在i的楼房高了，直接跳出，节省时间
+                    {
+                        break;
+                    }
                 }
             }
         }
