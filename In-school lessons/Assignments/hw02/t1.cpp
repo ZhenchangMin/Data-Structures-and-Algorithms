5e8d994e3e159f5587efed443f301a0688f86876
241880334 <241880334@smail.nju.edu.cn>
2025-09-20 16:07:53 +0800

    Update assignments.

--- In-school lessons/Assignments/hw02/t1.cpp
+++ In-school lessons/Assignments/hw02/t1.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
+int getCount(vector<int>& a){
+    int n = a.size();
+    int count = 0;
+    
+    return count;
+}
 int main()
 {
+    int T;
+    cin >> T;
+    while(T--){
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+        int count = 0;
+        count = getCount(a);
+        cout << count << '\n';
+    }
     return 0;
 }
