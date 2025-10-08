a886314f68842f504517c6a5d4d6ef4d1088d02c
241880334 <241880334@smail.nju.edu.cn>
2025-10-08 15:39:08 +0800

    Update assignments.

--- In-school lessons/Assignments/hw03/t1.cpp
+++ In-school lessons/Assignments/hw03/t1.cpp
@@ -1,50 +1,157 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
 using namespace std;
 
+int pushId = 1;
 
-struct Node{
-    int value;
-    Node *parent;
-    vector<Node*> children;
-};
-struct Tree{
-    Node *root;
-    vector<Node *> nodes;
-};
-
-class minHeap {
+class minHeap
+{
 public:
-    Tree heap;
+    vector<pair<int, int>> heap;
+    vector<int> pos; // hash set, pos[pushId] is the index of pushId in heap
     int size;
-    minHeap(){
+    minHeap()
+    {
         this->size = 0;
+        heap.push_back({0, 0});
+        pos.push_back(0); // To make indices start from 1
     }
-    minHeap(int size) {
+    minHeap(int size)
+    {
         this->size = size;
-        heap.nodes.resize(size);
+        heap.resize(size);
     }
     void push(int val);
     int pop();
     int top();
-    int size(){
+    int getSize()
+    {
         return size;
     }
     void decrease(int i, int k);
+
+private:
+    void heap_swap(int a, int b);
 };
 
-void minHeap::push(int val){
-
+void minHeap::heap_swap(int a, int b)
+{
+    swap(heap[a], heap[b]);
+    pos[heap[a].second] = a;
+    pos[heap[b].second] = b;
 }
 
-int minHeap::pop(){
-
+void minHeap::push(int val)
+{
+    heap.push_back({val, pushId});
+    if (pos.size() <= pushId)
+    {
+        pos.resize(pushId + 1, -1);
+    }
+    pos[pushId] = size + 1;
+    this->size++;
+    pushId++;
+    int index = size;
+    while (index > 1 && heap[index].first < heap[index / 2].first)
+    {
+        heap_swap(index, index / 2);
+        index /= 2;
+    }
 }
 
-int minHeap::top(){
+int minHeap::pop()
+{
+    if (size == 0)
+        return -1;
 
+    int result = heap[1].first;
+    pos[heap[1].second] = -1;
+
+    if (size == 1)
+    {
+        heap.pop_back();
+        size--;
+        return result;
+    }
+    heap_swap(1, size);
+    heap.pop_back();
+    size--;
+
+    int index = 1;
+    while (index * 2 <= size)
+    {
+        int left = index * 2;
+        int right = index * 2 + 1;
+        int smaller = left;
+        if (right <= size && heap[right].first < heap[left].first)
+        {
+            smaller = right;
+        }
+        if (heap[index].first <= heap[smaller].first)
+        {
+            break;
+        }
+        heap_swap(index, smaller);
+        index = smaller;
+    }
+    return result;
 }
 
-void minHeap::decrease(int i, int k){
+int minHeap::top()
+{
+    if (size == 0)
+        return -1;
+    return heap[1].first;
+}
 
+void minHeap::decrease(int i, int k) // must maintain a hash table to store pushId or would get TLE
+{
+    if (i >= pos.size() || pos[i] == -1)
+        return;
+
+    int index = pos[i];
+    heap[index].first = k;
+
+    while (index > 1 && heap[index].first < heap[index / 2].first)
+    {
+        heap_swap(index, index / 2);
+        index /= 2;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    minHeap heap = minHeap();
+    string op;
+    while (cin >> op)
+    {
+        if (op == "push")
+        {
+            int val;
+            cin >> val;
+            heap.push(val);
+        }
+        else if (op == "pop")
+        {
+            heap.pop();
+        }
+        else if (op == "top")
+        {
+            cout << heap.top() << '\n';
+        }
+        else if (op == "size")
+        {
+            cout << heap.getSize() << '\n';
+        }
+        else if (op == "decrease")
+        {
+            int index, newVal;
+            cin >> index >> newVal;
+            heap.decrease(index, newVal);
+        }
+    }
+    return 0;
 }
