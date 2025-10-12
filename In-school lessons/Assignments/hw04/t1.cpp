1e4c391fe28145d5c15545f888bd4a7da03afc3c
241880334 <241880334@smail.nju.edu.cn>
2025-10-13 07:21:20 +0800

    Update t1.

---
+++ In-school lessons/Assignments/hw04/t1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ull = unsigned long long;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while(T--){
+        int n;
+        cin >> n;
+        vector<ull> a(n);
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+    }
+    return 0;
+}
