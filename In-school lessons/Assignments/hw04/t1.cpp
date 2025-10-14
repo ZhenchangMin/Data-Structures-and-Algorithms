a648bc8f46720361c69ba234c25aa9d3dca16bab
ZhenchangMin <241880334@smail.nju.edu.cn>
2025-10-14 20:00:25 +0800

    Update t1.

--- In-school lessons/Assignments/hw04/t1.cpp
+++ In-school lessons/Assignments/hw04/t1.cpp
@@ -3,8 +3,35 @@
 using namespace std;
 using ull = unsigned long long;
 
-void mergeSort(vector<ull> &a){
-    
+vector<ull> mergeSort(vector<ull> a, ull left, ull right){
+    if (left >= right) {
+        return {a[left]}; // Return single element as vector
+    }
+
+    ull mid = left + (right - left) / 2;
+    vector<ull> leftPart = mergeSort(a, left, mid);
+    vector<ull> rightPart = mergeSort(a, mid + 1, right);
+    // merge
+    vector<ull> temp;
+    temp.reserve(leftPart.size() + rightPart.size());
+    ull i = 0, j = 0, k = 0;
+    while (i < leftPart.size() && j < rightPart.size()) {
+        cout << "? " << leftPart[i] << ' ' << rightPart[j] << '\n';
+        char res;
+        cin >> res;
+        if (res == '<') {
+            temp[k++] = leftPart[i++];
+        } else {
+            temp[k++] = rightPart[j++];
+        }
+    }
+    while (i < leftPart.size()) {
+        temp[k++] = leftPart[i++];
+    }
+    while (j < rightPart.size()) {
+        temp[k++] = rightPart[j++];
+    }
+    return temp;
 }
 
 int main(){
@@ -20,7 +47,7 @@ int main(){
         for(int i = 0; i < n; i++){
             cin >> a[i];
         }
-        mergeSort(a);
+        a = mergeSort(a, 0, n - 1);
         cout << '!' << '\n';
         for(const auto &x : a){
             cout << x << ' ';
