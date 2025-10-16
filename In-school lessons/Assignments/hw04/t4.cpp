b0b852ac08f160d443f374cbb79cf00cd6909e4a
241880334 <241880334@smail.nju.edu.cn>
2025-10-16 16:07:01 +0800

    update t4.

--- In-school lessons/Assignments/hw04/t4.cpp
+++ In-school lessons/Assignments/hw04/t4.cpp
@@ -2,9 +2,15 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <queue>
 using namespace std;
 using ll = long long;
 
 int main()
 {
+    ll n;
+    cin >> n;
+    queue<ll> a;
+    for (ll i = 1; i <= n; i++)
+        a.push(i);
 }
