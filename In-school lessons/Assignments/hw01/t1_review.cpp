9ea18f047b6b02cf08718c75e3d4c34459064755
241880334 <241880334@smail.nju.edu.cn>
2025-10-20 19:17:21 +0800

    Update assignments.

---
+++ In-school lessons/Assignments/hw01/t1_review.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    cin >> n >> m;
+    deque<string> planes;
+    while (n--)
+    {
+        int op;
+        cin >> op;
+        switch (op)
+        {
+        case 1:
+        {
+            string number;
+            cin >> number;
+            if (planes.size() < m)
+            {
+                planes.push_back(number);
+            }
+            else
+            {
+                cout << "full" << '\n';
+            }
+            break;
+        }
+        case 2:
+        {
+            if (planes.empty())
+            {
+                cout << "empty" << '\n';
+            }
+            else
+            {
+                string fly = planes[0];
+                planes.pop_front();
+                cout << fly << '\n';
+            }
+            break;
+        }
+        case 3:
+        {
+            cout << planes.size() << '\n';
+            break;
+        }
+        }
+    }
+    return 0;
+}
--- In-school lessons/Assignments/hw05/t2.cpp
+++ In-school lessons/Assignments/hw05/t2.cpp
@@ -20,7 +20,6 @@ int main()
         for (int i = curr_index + 1; i <= n; i += 2)
         {
             cout << '?' << ' ' << curr_index << ' ' << i << endl;
-            cout.flush();
             cin >> hasEdge[curr_index][i];
             total += hasEdge[curr_index][i];
             hasEdgeBetween.push_back({curr_index, i});
@@ -29,7 +28,6 @@ int main()
         if (total == 0 || total > 1)
         {
             cout << "! 1" << endl;
-            cout.flush();
             continue;
         }
         int left = hasEdgeBetween[0].first;
@@ -39,37 +37,29 @@ int main()
         int rightForward = right + 1 <= n ? right + 1 : 1;
         int rightForwardTwo = rightForward + 1 <= n ? rightForward + 1 : 1;
         cout << '?' << ' ' << leftForward << ' ' << left << endl;
-        cout.flush();
         int leftForwardResult;
         cin >> leftForwardResult;
         cout << '?' << ' ' << rightForward << ' ' << right << endl;
-        cout.flush();
         int rightForwardResult;
         cin >> rightForwardResult;
         if (leftForwardResult == 0 && rightForwardResult == 0)
         {
             cout << "! 1" << endl;
-            cout.flush();
             continue;
         }
         int possibleRoot = leftForwardResult == 1 ? left : right;
-        int rootForward = leftForwardResult == 1 ? leftForward - 1 : rightForward + 1;
-        rootForward = rootForward > 0 ? rootForward : n;
-        rootForward = rootForward <= n ? rootForward : 1;
+        int rootForward = leftForwardResult == 1 ? leftForwardTwo: rightForwardTwo;
         cout << '?' << ' ' << possibleRoot << ' ' << rootForward << endl;
-        cout.flush();
         int rootForwardResult;
         cin >> rootForwardResult;
         if (rootForwardResult == 1)
         {
             cout << "! 2" << endl;
-            cout.flush();
             continue;
         }
         else
         {
             cout << "! 1" << endl;
-            cout.flush();
             continue;
         }
     }
