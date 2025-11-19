4fd892fd640cddef621f2a148dc5763dcf2f241c
ZhenchangMin <728262392@qq.com>
2025-11-19 10:53:01 +0800

    Update assignments.

--- In-school lessons/Assignments/hw08/t2.cpp
+++ In-school lessons/Assignments/hw08/t2.cpp
@@ -1,36 +1,108 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <climits>
-
+#include <queue>
+#include <cstring>
 using namespace std;
-using ll = long long;
 
-struct DSU {
-    vector<int> parent;
-    vector<int> edge_idx;
-    DSU(int n) : parent(n + 1), edge_idx(n + 1, -1) {
-        for (int i = 0; i <= n; ++i)
-            parent[i] = i;
-    }
-    pair<int, vector<int>> find(int u) {
-        vector<int> path;
-        while (parent[u] != u) {
-            path.push_back(edge_idx[u]);
-            u = parent[u];
+const int MAXN = 100005;
+
+int n, m;
+vector<pair<int, int>> adj[MAXN];
+int parent[MAXN];
+int dist[MAXN];
+bool vis[MAXN];
+
+int dsu[MAXN];
+int find(int x) {
+    return dsu[x] == x ? x : (dsu[x] = find(dsu[x]));
+}
+bool unite(int x, int y) {
+    x = find(x), y = find(y);
+    if (x == y) return false;
+    dsu[x] = y;
+    return true;
+}
+
+vector<int> bfs_path(int start, int end) {
+    queue<int> q;
+    q.push(start);
+    memset(vis, 0, sizeof(vis));
+    memset(parent, -1, sizeof(parent));
+    vis[start] = true;
+    dist[start] = 0;
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        if (u == end) break;
+        for (auto& e : adj[u]) {
+            int v = e.first, idx = e.second;
+            if (!vis[v]) {
+                vis[v] = true;
+                parent[v] = u;
+                dist[v] = dist[u] + 1;
+                q.push(v);
+            }
         }
-        return {u, path};
     }
-    bool unite(int u, int v, int idx) {
-        auto [root_u, path_u] = find(u);
-        auto [root_v, path_v] = find(v);
-        if (root_u == root_v)
-            return false;
-        parent[root_v] = root_u;
-        edge_idx[root_v] = idx;
-        return true;
+    vector<int> edges;
+    int cur = end;
+    while (parent[cur] != -1) {
+        int p = parent[cur];
+        for (auto& e : adj[cur]) {
+            if (e.first == p) {
+                edges.push_back(e.second);
+                break;
+            }
+        }
+        cur = p;
     }
-};
+    return edges;
+}
+
+void solve() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        adj[i].clear();
+        dsu[i] = i;
+    }
+    vector<pair<int, int>> edges(m);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        edges[i] = {u, v};
+    }
+
+    int best_max_idx = m + 1;
+    vector<int> best_cycle;
+    bool found = false;
+
+    for (int i = 0; i < m; i++) {
+        int u = edges[i].first, v = edges[i].second;
+        if (find(u) == find(v)) {
+            vector<int> cycle_edges = bfs_path(u, v);
+            cycle_edges.push_back(i + 1);
+            best_max_idx = i + 1;
+            best_cycle = cycle_edges;
+            found = true;
+            break;
+        } else {
+            unite(u, v);
+            adj[u].push_back({v, i + 1});
+            adj[v].push_back({u, i + 1});
+        }
+    }
+
+    if (!found) {
+        cout << -1 << "\n";
+    } else {
+        sort(best_cycle.begin(), best_cycle.end());
+        for (size_t i = 0; i < best_cycle.size(); i++) {
+            if (i > 0) cout << " ";
+            cout << best_cycle[i];
+        }
+        cout << "\n";
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -38,39 +110,7 @@ int main() {
     int T;
     cin >> T;
     while (T--) {
-        int n, m;
-        cin >> n >> m;
-        vector<pair<int, int>> edges(m + 1);
-        for (int i = 1; i <= m; i++) {
-            cin >> edges[i].first >> edges[i].second;
-        }
-        DSU dsu(n);
-        vector<int> ans;
-        bool found = false;
-        for (int i = 1; i <= m; i++) {
-            int u = edges[i].first;
-            int v = edges[i].second;
-            auto [root_u, path_u] = dsu.find(u);
-            auto [root_v, path_v] = dsu.find(v);
-            if (root_u == root_v) {
-                vector<int> ring = path_u;
-                ring.insert(ring.end(), path_v.begin(), path_v.end());
-                ring.push_back(i);
-                sort(ring.begin(), ring.end());
-                for (size_t j = 0; j < ring.size(); j++) {
-                    if (j > 0) cout << ' ';
-                    cout << ring[j];
-                }
-                cout << '\n';
-                found = true;
-                break;
-            } else {
-                dsu.unite(u, v, i);
-            }
-        }
-        if (!found) {
-            cout << "-1\n";
-        }
+        solve();
     }
     return 0;
 }
---
+++ In-school lessons/Assignments/hw08/t3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n, m;
+    cin >> n >> m;
+    
+
+    return 0;
+}
