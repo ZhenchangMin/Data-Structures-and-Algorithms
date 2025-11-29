1c0c4922a0d11ee86e846980db93c7cdf3730e65
ZhenchangMin <728262392@qq.com>
2025-11-29 20:24:07 +0800

    Update assignments.

--- In-school lessons/Assignments/hw09/t3.cpp
+++ In-school lessons/Assignments/hw09/t3.cpp
@@ -1,14 +1,85 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <deque>
 using namespace std;
 using ll = long long;
 
 int main(){
-    int T;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll T;
     cin >> T;
-    while(T--){
-        
+    ll ans = 0;
+    for (ll t = 1; t <= T; t++) {
+        ll n, m;
+        cin >> n >> m;
+        vector<vector<ll>> board(n, vector<ll>(m));
+        for (ll i = 0; i < n; i++) {
+            for (ll j = 0; j < m; j++) {
+                cin >> board[i][j];
+            }
+        }
+
+        int N = n + m;
+        bool possible = true;
+        vector<char> seen;
+        seen.assign((size_t)N * (size_t)N, 0);
+
+        vector<vector<int>> adj(N);
+        vector<int> indeg(N, 0);
+
+        for (int i = 0; i < n && possible; ++i) {
+            for (int j = 0; j < m; ++j) {
+                int val = board[i][j];
+                int ri = i + 1;
+                int cj = n + j + 1;
+
+                if (val != ri && val != cj) {
+                    possible = false;
+                    break;
+                }
+                if (val == ri) {
+                    int u = n + j;
+                    int v = i;
+                    size_t key = (size_t)u * N + v;
+                    if (!seen[key]) {
+                        seen[key] = 1;
+                        adj[u].push_back(v);
+                        indeg[v] += 1;
+                    }
+                } else {
+                    int u = i;
+                    int v = n + j;
+                    size_t key = (size_t)u * N + v;
+                    if (!seen[key]) {
+                        seen[key] = 1;
+                        adj[u].push_back(v);
+                        indeg[v] += 1;
+                    }
+                }
+            }
+        }
+
+        if (!possible) continue;
+        deque<int> dq;
+        for (int v = 0; v < N; ++v) {
+            if (indeg[v] == 0) dq.push_back(v);
+        }
+        int visited = 0;
+        while (!dq.empty()) {
+            int u = dq.front(); dq.pop_front();
+            ++visited;
+            for (int v : adj[u]) {
+                --indeg[v];
+                if (indeg[v] == 0) dq.push_back(v);
+            }
+        }
+
+        if (visited == N) {
+            ans += t;
+        }
     }
+    cout << ans << "\n";
     return 0;
 }
--- In-school lessons/Assignments/hw09/t4.cpp
+++ In-school lessons/Assignments/hw09/t4.cpp
@@ -1,14 +1,74 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
-int main(){
-    int T;
-    cin >> T;
-    while(T--){
-        
+struct UnionFind {
+    vector<int> parent;
+    UnionFind(int n) {
+        parent.resize(n);
+        for (int i = 0; i < n; ++i) parent[i] = i;
     }
-    return 0;
+    int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    bool unite(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) return false;
+        parent[y] = x;
+        return true;
+    }
+};
+
+__int128 lcm128(__int128 a, __int128 b) {
+    return a / gcd((long long)a, (long long)b) * b;
+}
+
+void print128(__int128 x) {
+    if (x == 0) { cout << 0; return; }
+    string s;
+    while (x) {
+        s.push_back('0' + x % 10);
+        x /= 10;
+    }
+    reverse(s.begin(), s.end());
+    cout << s;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long l, r;
+    cin >> l >> r;
+
+    int n = r - l + 1;
+    if (n == 1) {
+        cout << 0 << "\n";
+        return 0;
+    }
+
+    vector<tuple<__int128,int,int>> edges;
+    for (int i = 0; i < n-1; ++i) {
+        __int128 a = l + i;
+        __int128 b = l + i + 1;
+        edges.push_back({lcm128(a,b), i, i+1});
+    }
+
+    sort(edges.begin(), edges.end());
+    UnionFind uf(n);
+    __int128 ans = 0;
+    int cnt = 0;
+    for (auto &e : edges) {
+        __int128 w; int u,v;
+        tie(w,u,v) = e;
+        if (uf.unite(u,v)) {
+            ans += w;
+            cnt++;
+            if (cnt == n-1) break;
+        }
+    }
+
+    print128(ans);
+    cout << "\n";
 }
